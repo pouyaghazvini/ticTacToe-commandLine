3784df00fe9459274b61bd30e17b779bb6642543
Pouya Ghazvini <pouyaghazvini@gmail.com>
2018-12-12 14:17:41 -0800

    changed name of array

--- game.js
+++ game.js
@@ -24,7 +24,7 @@ logBoard = () => {
         ' ' + board[7] + ' | ' + board[8] + ' | ' + board[9] + '\n');
 }
 
-const allPossibleCombinations = [
+const allWinningCombinations = [
   [1, 2, 3], [4, 5, 6], [7, 8, 9], 
   [1, 4, 7], [2, 5, 8], [3, 6, 9], 
   [1, 5, 9], [3, 5, 7]
